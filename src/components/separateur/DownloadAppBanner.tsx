e967a6b651bcef5cd3c0adbdc88892597e8e26d0
Axel NGuessan <nguessanaxel21@gmail.com>
2025-08-20 15:16:03 +0000

    v14

--- src/components/separateur/DownloadAppBanner.tsx
+++ src/components/separateur/DownloadAppBanner.tsx
@@ -6,21 +6,21 @@ const DownloadAppBanner = () => {
      lg:px-10 xl:px-16 xl:pb-2.5 2xl:pb-2.5 lg:pb-2.5 md:pb-2.5 my-5 max-sm:pb-1 shadow-2xl">
             <h1 className="text-left font-medium">Télécharger l'applicaton{""} <span className="font-bold">YeshouaApp</span></h1>
        
-            <button onClick={() => {}} className="bg-white border border-gray-800 shadow-xl flex flex-row items-center justify-center gap-x-1 px-2 rounded-lg cursor-pointer
+            <button onClick={() => {}} className="bg-gray-800 border border-gray-800 shadow-xl flex flex-row items-center justify-center gap-x-1 px-2 rounded-lg cursor-pointer
             hover:scale-105 transition-transform duration-300">
-                <img src="/playStore.svg" alt="Google Play" className="h-5" />
+                <img src="/playStoreBlanc.svg" alt="Google Play" className="h-5" />
                 <div className='flex flex-col items-start justify-start'>
-                    <span className="text-gray-800 text-xs">Télécharger l'application</span>
-                    <span className="text-gray-800 text-lg font-semibold">Google Play</span>
+                    <span className="text-white text-xs">Télécharger l'application</span>
+                    <span className="text-white text-lg font-semibold">Google Play</span>
                 </div>
             </button>
 
-            <button onClick={() => {}} className="bg-white border border-gray-800 shadow-xl flex flex-row items-center justify-center gap-x-1 px-2 rounded-lg cursor-pointer
+            <button onClick={() => {}} className="bg-gray-800 border border-gray-400 shadow-xl flex flex-row items-center justify-center gap-x-1 px-2 rounded-lg cursor-pointer
             hover:scale-105 transition-transform duration-300">
-                <img src="/appleStore.svg" alt="App Store" className="h-5" />
+                <img src="/appleStoreBlanc.svg" alt="App Store" className="h-5" />
                 <div className='flex flex-col items-start justify-start'>
-                    <span className="text-gray-800 text-xs">Télécharger l'application</span>
-                    <span className="text-gray-800 text-lg font-semibold">App Store</span>
+                    <span className="text-white text-xs">Télécharger l'application</span>
+                    <span className="text-white text-lg font-semibold">App Store</span>
                 </div>
             </button>
      
