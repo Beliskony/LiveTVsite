4c0723f223f29e12f2662fc7ef64cbdce5f3f301
Axel NGuessan <nguessanaxel21@gmail.com>
2025-08-21 08:45:25 +0000

    Update VideoCard.tsx

--- src/components/mediaComponent/VideoCard.tsx
+++ src/components/mediaComponent/VideoCard.tsx
@@ -1,37 +1,17 @@
 import React, {Suspense, lazy} from "react"
 
 import { formatRelativeDate } from "@/utilitaires/FormatDate"
-import { useState } from "react"
 import type { IVideo } from "@/interfaces/Videos"
-import { Play, Clock } from "lucide-react"
+import { Clock } from "lucide-react"
 import { Card, CardContent } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
-import { videosData } from "@/data/videosData"
+
 
 const VideoModal = lazy(() => import("@/components/mediaComponent/VideoModal"))
 
 export const VideoCard = (video: IVideo) => {
   
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const openModal = () => setIsModalOpen(true)
-  const closeModal = () => setIsModalOpen(false)
-
-  const handleOpen = (e: React.MouseEvent) => {
-  e.stopPropagation();
-  const vid = e.currentTarget
-    .closest("div")
-    ?.querySelector("video") as HTMLVideoElement | null;
-  if (vid) {
-    vid.pause();
-    vid.currentTime = 0; // reset
-  }
-  openModal();
-};
-
-  const { id, title, duration, createdAt, videoUrl, } = video;
-  const emptyVideo: IVideo = { id: "", title: "Vidéo non trouvée", description: "", createdAt: new Date(), duration: "", views: 0, Miniature: "", category: "", status: "draft", videoUrl: "", };
-
+  const { title, duration, createdAt, videoUrl, } = video;
   
   return (
     <>
@@ -43,44 +23,35 @@ export const VideoCard = (video: IVideo) => {
           src={videoUrl}
           controls
           preload="metadata"
-          muted
+          muted={false}
           loop
           playsInline
           onMouseEnter={(e) => e.currentTarget.play()}
           onMouseLeave={(e) =>{ e.currentTarget.pause(); e.currentTarget.currentTime = 0; }}
           className="w-full h-full max-h-[80vh] object-contain transition-transform duration-300 group-hover:scale-105"
         />
-
-        {/* Duration badge */}
-        {duration && (
-          <Badge variant="secondary" className="absolute bottom-2 right-2 bg-black/80 text-white hover:bg-black/80">
-            <Clock className="mr-1 h-3 w-3" />
-            {duration}
-          </Badge>
-        )}
-
  
       </div>
 
       <CardContent className="w-full py-2 space-y-1 h-20 text-gray-800">
 
-        <div className="flex items-center gap-4 text-sm text-muted-foreground">
+        <div className="flex justify-between items-center gap-4 text-sm text-muted-foreground">
             <span>{formatRelativeDate(createdAt.toLocaleDateString())}</span>
-          </div>
+            {/* Duration badge */}
+         {duration && (
+          <Badge variant="secondary" className=" bg-black/80 text-white hover:bg-black/80">
+            <Clock className="mr-1 h-3 w-3" />
+            {duration}
+          </Badge>
+          )}
+        </div>
         {/* Title */}
         <h3 className="text-wrap text-sm leading-tight line-clamp-2 font-extrabold transition-colors">
           {title}
         </h3>
 
       </CardContent>
-    </Card>
-
-    
-      <Suspense fallback={<div className="p-6 text-center text-gray-400">Chargement...</div>}>
-        {isModalOpen && (
-          <VideoModal video={videosData.find((video) => video.id === id) ?? emptyVideo} isOpen={isModalOpen} onClose={closeModal} />
-        )}
-      </Suspense>
-    </>
+    </Card>    
+</>
   )
 }
